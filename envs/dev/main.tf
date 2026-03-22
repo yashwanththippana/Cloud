9f1cddc452db7a28a37edfcd37d29ee333ff1e6b
yash_reddy <65530416+yashwanththippana@users.noreply.github.com>
2026-03-22 18:45:44 -0400

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,10 +1,18 @@
-variable "service" {
-  type = string
+terraform {
+  required_version = ">= 1.0.0"
 }
 
+provider "aws" {
+  region = var.region
+}
+
+# SNS Module (only runs when service = sns)
 module "sns" {
-  source = "../../modules/sns"
-  name   = "my-sns-topic"
   count  = var.service == "sns" ? 1 : 0
-}
+  source = "../../modules/sns"
 
+  name                        = var.sns_topic_name
+  email_subscription          = var.sns_email_subscription
+  fifo_topic                  = var.sns_fifo
+  content_based_deduplication = var.sns_dedup
+}
